ef7dc1fb369bf4d4776fa80b1e3bc68977f7b27d
Fassih Fayyaz <fassihfayyaz@gmail.com>
2024-04-21 02:18:59 +0500

    Basic Setup Install Script

---
+++ Install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+read -p "Install Xorg and Qtile? (y/n) " install_wm
+if [[ $install_wm == "y" || $install_wm == "Y" ]]; then
+    sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-apps
+    sudo mkdir ~/.xinitrc
+    echo "exec /usr/bin/qtile start" >> ~/.xinitrc
+    sudo pacman -S --noconfirm qtile alacritty thunar
+    sudo pacman -S --noconfirm python-psutil
+    yay -S --noconfirm qtile-extras
+    sudo pacman -S --noconfirm python-pywal
+fi
+
+read -p "Install AUR Package Manager? (y/n) " install_aur
+if [[ $install_aur == "y" || $install_aur == "Y" ]]; then
+    sudo pacman -S --noconfirm base-devel
+    cd ~/Downloads || mkdir ~/Downloads && cd ~/Downloads
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+fi
+
+read -p "Setup Fast Mirrors? (y/n) " setup_mirrors
+if [[ $setup_mirrors == "y" || $setup_mirrors == "Y" ]]; then
+    yay -S --noconfirm rate-mirrors
+    rate-mirrors arch | sudo tee /etc/pacman.d/mirrorlist
+fi
+
+read -p "Install Wayland? (y/n) " install_wayland
+if [[ $install_wayland == "y" || $install_wayland == "Y" ]]; then
+    sudo pacman -S --noconfirm python-pywlroots xorg-xwayland
+fi
+
+read -p "Install Important Programs? (y/n) " install_programs
+if [[ $install_programs == "y" || $install_programs == "Y" ]]; then
+    sudo pacman -S --noconfirm git fastfetch ntfs-3g xdg-user-dirs python-pywalfox picom rofi thunar thunar-archive-plugin xarchiver unrar unzip stow obsidian mousepad vlc dunst starship mesa-utils alsa-utils
+    xdg-user-dirs-update
+fi
+
+# read -p "Set Fstab Permissions? (y/n) " set_fstab
+# if [[ $set_fstab == "y" || $set_fstab == "Y" ]]; then
+#     echo "uid=1000,gid=1000,rw,user,exec,umask=000" | sudo tee -a /etc/fstab
+# fi
+
+read -p "Install Fonts and Apps before adding dotfiles? (y/n) " install_pre_dotfiles
+if [[ $install_pre_dotfiles == "y" || $install_pre_dotfiles == "Y" ]]; then
+    sudo pacman -S --noconfirm ttf-jetbrains-mono-nerd ttf-liberation sxiv firefox firefox-i18n-ur rofi-emoji rofi-calc xdotool btop eza zoxide fzf polkit-gnome thorium-browser-bin 
+fi
+
+read -p "Setup Dotfiles? (y/n) " setup_dotfiles
+if [[ $setup_dotfiles == "y" || $setup_dotfiles == "Y" ]]; then
+    cd ~/Downloads || mkdir ~/Downloads && cd ~/Downloads
+    git clone https://github.com/FassihFayyaz/dotfiles.git
+    cd dotfiles/
+    cp -r dotfiles/ ~/
+    cd ~/dotfiles
+    stow . --adopt
+fi
+
+read -p "Setup Themes? (y/n) " setup_themes
+if [[ $setup_themes == "y" || $setup_themes == "Y" ]]; then
+    sudo pacman -S --noconfirm gnome-themes-extra nwg-look papirus-icon-theme 
+    yay -S --noconfirm adwaita-qt5 adwaita-qt6 qt5ct qt6ct
+fi
+
+read -p "Install Optional Softwares? (y/n) " install_important
+if [[ $install_important == "y" || $install_important == "Y" ]]; then
+    sudo pacman -S --noconfirm corectrl kodi flameshot noisetorch-bin copyq qbittorrent polychromatic linux-headers
+    yay -S --noconfirm vesktop green-tunnel
+fi
+
+read -p "Setup Gaming? (y/n) " setup_gaming
+if [[ $setup_gaming == "y" || $setup_gaming == "Y" ]]; then
+    sudo pacman -S --noconfirm steam lutris goverlay mangohud gamemode wine-staging
+    yay -S --noconfirm protonup-qt-bin
+fi
---
+++ README.md
@@ -0,0 +1,25 @@
+# Arch Linux Installation Script
+
+This is a bash script to automate the installation process of various packages and configurations on Arch Linux. The script prompts the user for each section, allowing them to choose whether to install or skip that section.
+
+## Features
+
+- Install Window Manager (Qtile) and Xorg
+- Install AUR package manager (yay)
+- Install Wayland
+- Install essential programs and utilities
+- Set FSTAB permissions
+- Setup fast mirrors
+- Install apps before adding dotfiles
+- Setup dotfiles
+- Install themes and appearance-related packages
+- Install sound utilities
+- Install important softwares (CoreCtrl, Kodi, Flameshot, etc.)
+- Setup gaming environment (Steam, Lutris, Wine, etc.)
+
+## Usage
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/your-username/arch-install-script.git
