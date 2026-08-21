197d496e1ddbf1940654bcda7e9a54fc76fecba8
FassihFayyaz <fassihfayyaz@gmail.com>
2026-08-21 09:14:53 +0500

    Enable power-profiles-daemon after installing

--- install.sh
+++ install.sh
@@ -89,6 +89,8 @@ read -p "Install essential applications? (y/n) " -r
 if [[ $REPLY == "y" || $REPLY == "Y" ]]; then
     echo ":: Installing essential apps"
     sudo pacman -S --noconfirm fastfetch thunar zed opencode github-cli btop polkit-gnome power-profiles-daemon
+    echo ":: Enabling power-profiles-daemon"
+    sudo systemctl enable --now power-profiles-daemon
     echo ":: Installing brave-origin-bin and vesktop-bin"
     sudo pacman -S --noconfirm brave-origin-bin vesktop-bin
 fi
